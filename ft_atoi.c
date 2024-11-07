018e7ff78518b4e67e1278ea2d0c5b3a59942644
Ikram Dahhan <idahhan@e2r8p9.1337.ma>
2024-11-07 22:09:51 +0100

    -----

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: idahhan <idahhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 18:05:22 by idahhan           #+#    #+#             */
-/*   Updated: 2024/11/07 21:59:26 by idahhan          ###   ########.fr       */
+/*   Updated: 2024/11/07 22:06:41 by idahhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	ft_atoi(const char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (res < LONG_MAX - (str[i] - '0') / 10)
+		if (res > LONG_MAX - (str[i] - '0') / 10)
 			return (ft_overflow(sign));
 		res = res * 10 + (str[i] - '0');
 		i++;
